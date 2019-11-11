cdedbcf36158913401de079239a00c74b3ca954a
Ashwin Mahesh <mahesh2@mc03.cs.purdue.edu>
2019-11-11 16:47:27 -0500

    TFIDF successfully being calculated

--- clustering.py
+++ clustering.py
@@ -1,10 +1,50 @@
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer 
 from XMLParser import XMLParse
+import numpy as np
+import time
+import math
+
+class TFIDF:
+  def __init__(self, allTFs, docCount):
+    self.docCount = docCount
+    self.allTFs = np.array(allTFs)
+    self.idf = np.zeros(len(self.allTFs[0]))
+    self.tfidf=[]
+
+    self.calculateIDF()
+    self.calculateTFIDF()
+  
+  #Calculates the IDF score based on inputted TF
+  def calculateIDF(self):
+    counts = np.zeros(len(self.allTFs[0]))
+    for tf in self.allTFs:
+      containedIndexes = np.where(tf>0)
+      for index in containedIndexes:
+        counts[index]+=1
+    
+    for i in range(0, len(counts)):
+      self.idf[i] = math.log(self.docCount / (counts[i]), 2)
+
+  #Calculates TFIDF Based on Inputted TF and IDF
+  def calculateTFIDF(self):
+    for i in range(0, self.docCount):
+      self.tfidf.append(np.multiply(self.allTFs[i], self.idf))
+
+  #Prints the value in any of the three fields
+  def printVal(self, valName, index=0):
+    np.set_printoptions(threshold=np.inf)
+    if(valName == 'tfidf'):
+      print(self.tfidf[index])
+    elif valName == 'tf':
+      print(self.allTFs[index])
+    elif valName == 'idf':
+      print(self.idf)
+
 
 #Checks if the inputted string is a number, special in that commas do not make it not a number
 def isNumber(string):
@@ -44,7 +84,7 @@ def findWordIndex(uniqueWordList, word):
   except ValueError:
     return -1
 
-#Takes in a stemmed and stopped document, along the list of all the words in the corpus, and returns the TF Vector
+#Takes in a stemmed and stopped document, along the list of all the words in the corpus, and returns the TF Vector. Calculates a running IDF
 def calculateTermFrequency(document, uniqueWordList):
   tfOutput=[0] * len(uniqueWordList)
   docWords = document.getField('BODY').split()
@@ -54,28 +94,54 @@ def calculateTermFrequency(document, uniqueWordList):
       tfOutput[index]+=1
   return tfOutput
 
-def calculateIDF()
 
 if __name__ == '__main__':
-  print("\nAgglomerative Clustering by Ashwin Mahesh\n")
+  startTime = time.time()
+
+  print("\nAgglomerative Clustering by Ashwin Mahesh (@mahesh2)\n")
+
   print("Extracting data from XML Document...")
-  values = XMLParse("/homes/cs473/project2/reut2-subset.sgm")
+  values = XMLParse("/homes/cs473/project2/reut2-subset.sgm", 100, True)
+  print("Number of Documents: "+str(len(values)))
+  extractionTime = round(time.time() - startTime, 3)
+  print("Time: " + str(extractionTime) + " seconds")
+
   print("Removing stopwords and stemming...")
   for i in range(len(values)-1, -1, -1):
     if values[i].hasField('BODY'):
       values[i].setField('BODY',removeStopwords(values[i].getField("BODY")))
     else:
       del values[i]
-  
+  removingTime = round(time.time() - startTime - extractionTime, 3)
+  print("Time: " + str(removingTime) + " seconds")
+
   print("Creating list of all unique words in corpus...")
   uniqueWords = getUniqueWords(values)
+  uniqueWordsTime = round(time.time() - startTime - extractionTime - removingTime, 3)
+  print("Time: " + str(uniqueWordsTime) + " seconds")
 
-  for i in range(0, 5):
-    print('Iteration: '+str(i))
-    tf = calculateTermFrequency(values[i], uniqueWords)
-    printVal=''
-    for i in range(0, len(tf)):
-      if tf[i]>0:
-        printVal+=(uniqueWords[i]+': '+str(i) + '\t')
-    print(printVal)
+
+  print("Calculating all term frequencies...")
+  TFs=[]
+  for i in range(0, len(values)):
+    TFs.append(calculateTermFrequency(values[i], uniqueWords))
+  tfTime = round(time.time() - startTime - extractionTime - removingTime - uniqueWordsTime, 3)
+  print("Time: " + str(tfTime) + " seconds")
+
+  print("Creating TFIDF class and computing IDF...")
+  computedTFIDF = TFIDF(TFs, len(values))
+  # computedTFIDF.printVal('tfidf', 0)
+  idfTime = round(time.time() - startTime - extractionTime - removingTime - uniqueWordsTime - tfTime, 3)
+  print("Time: " + str(idfTime) + " seconds")
+
+
+  print('PROGRAM HAS TERMINATED EXECUTION')
+  print("Execution Time: " + str(round(time.time() - startTime, 3)) + " seconds")
+
+    # printVal=''
+    # for i in range(0, len(tf)):
+    #   if tf[i]>0:
+    #     printVal+=(uniqueWords[i]+': '+str(i) + '\t')
+    # print(printVal)
+    # print("IDF:", idf)
  
