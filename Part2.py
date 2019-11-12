a8e4b89990bc08d20ecfa0c059321e3a95e80b34
Ashwin Mahesh <mahesh2@mc03.cs.purdue.edu>
2019-11-11 21:40:23 -0500

    Renamed file and installed scipy

--- Part2.py
+++ Part2.py
@@ -1,9 +1,12 @@
 import time
+import scipy
 
 def part2(computedTFIDF):
   startTime = time.time()
 
   print("Executing code for Part 2...")
+
+  scipy.test()
 
   print('\nPart 2 Complete')
   print("Execution Time: " + str(round(time.time() - startTime, 3)) + " seconds\n")
--- main.py
+++ main.py
@@ -6,8 +6,9 @@ startTime = time.time()
 
 print("\nAgglomerative Clustering by Ashwin Mahesh (@mahesh2)\n")
 
-computedTFIDF = part1()
-part2(computedTFIDF)
+# computedTFIDF = part1()
+# part2(computedTFIDF)
+part2('a')
 
 print('PROGRAM HAS TERMINATED EXECUTION')
 print("Execution Time: " + str(round(time.time() - startTime, 3)) + " seconds")
