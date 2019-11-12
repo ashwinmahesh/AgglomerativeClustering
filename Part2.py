4dde50aab6c61d8a04c411f6ddcc771ea5cf6c71
Ashwin Mahesh <mahesh2@mc03.cs.purdue.edu>
2019-11-11 19:12:21 -0500

    Created part 2 file

---
+++ Part2.py
@@ -0,0 +1,9 @@
+import time
+
+def part2(computedTFIDF):
+  startTime = time.time()
+
+  print("Executing code for Part 2...")
+
+  print('\nPart 2 Complete')
+  print("Execution Time: " + str(round(time.time() - startTime, 3)) + " seconds\n")
--- clustering.py
+++ clustering.py
@@ -1,11 +1,13 @@
 import time
 from Part1 import part1
+from Part2 import part2
 
 startTime = time.time()
 
 print("\nAgglomerative Clustering by Ashwin Mahesh (@mahesh2)\n")
 
 computedTFIDF = part1()
+part2(computedTFIDF)
 
 print('PROGRAM HAS TERMINATED EXECUTION')
 print("Execution Time: " + str(round(time.time() - startTime, 3)) + " seconds")
