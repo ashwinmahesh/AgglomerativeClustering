2d2c71634350980463aaa759a9fa33f66a49f117
Ashwin Mahesh <mahesh2@mc17.cs.purdue.edu>
2019-11-12 22:06:11 -0500

    Added cleanup function to remove clusters that contain too many of the values

--- Part3.py
+++ Part3.py
@@ -1,9 +1,9 @@
 import time
 
-def extractUniqueTopics(parsedDocuments):
+def extractUniqueTopics(clusteringMethod):
   uniqueTopics={}
-  for doc in parsedDocuments:
-    topics = doc.extractSubElements('TOPICS')
+  for doc in clusteringMethod:
+    topics = doc['topics']
     for topic in topics:
       if topic not in uniqueTopics:
         uniqueTopics[topic]=0
@@ -18,6 +18,15 @@ def getAllDocsInEachCluster(clustering):
         allClusters[cluster]=[]
       allClusters[cluster].append(document['id'])
   return allClusters
+
+def cleanupClusters(clusterList, proportion, docCount):
+  removeValues=[]
+  for cluster in clusterList:
+    if len(clusterList[cluster])>proportion*docCount:
+      removeValues.append(cluster)
+  for value in removeValues:
+    del clusterList[value]
+  return clusterList
 
 def evaluate(clusteringMethod):
   return True
@@ -35,13 +44,16 @@ def part3(parsedDocuments, singleClustering, completeClustering):
   print("Executing code for Part 3...\n")
 
   print("Beginning Evaluation of Clustering...")
+  docsBySingleCluster = getAllDocsInEachCluster(singleClustering)
+  removingOvercrowdedSingle = cleanupClusters(docsBySingleCluster, 0.3, len(parsedDocuments))
+  print(removingOvercrowdedSingle)
   evalTime = round(time.time() - startTime, 3)
   runningTotalTime+=evalTime
   print("Time: " + str(evalTime) + " seconds")
 
   # print(getAllDocsInEachCluster(completeClustering))
   # test(singleClustering)
-  # print(extractUniqueTopics(parsedDocuments))
+  # print(extractUniqueTopics(singleClustering))
   # test(parsedDocuments)
 
   print('\nPart 3 Complete')
